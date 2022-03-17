a599e7a08b49a93dd89abfb6fcdc00206fe4d115
Kylaksizov <masterz1zzz@gmail.com>
2022-03-17 21:05:46 +0200

    Создание редактирование категорий 2

--- app/controllers/ajax/panel/CategoryShop.php
+++ app/controllers/ajax/panel/CategoryShop.php
@@ -25,7 +25,9 @@ class CategoryShop{
 
         if(empty($cid[1])){ // если это добавление новой категории
 
-            $CategoryModel->create($title, $meta, $cont, $url, $icon, $pid);
+            $id = $CategoryModel->create($title, $meta, $cont, $url, $icon, $pid);
+
+            if(!empty($_FILES["icon"])) $this->uploadIcon($id);
 
             $script = '<script>
                 $.server_say({say: "Категория создана!", status: "success"});
@@ -33,7 +35,10 @@ class CategoryShop{
 
         } else{ // если редактирование
 
+            $id = intval($cid);
             $CategoryModel->edit(intval($cid), $title, $meta, $cont, $url, $icon, $pid);
+
+            if(!empty($_FILES["icon"])) $this->uploadIcon($id);
 
             $script = '<script>
                 $("h1 b").text(`'.$title.'`);
@@ -44,6 +49,16 @@ class CategoryShop{
         System::script($script);
 
         return '155';
+    }
+
+
+    private function uploadIcon($id){
+
+        $dir = ROOT . '/uploads/products/' . date("Y-m", time());
+
+        if(!file_exists($dir)) mkdir($dir, 0777, true);
+
+        move_uploaded_file($_FILES["icon"]["tmp_name"], $dir . '/' . $_FILES["icon"]["name"]);
     }
 
 }
